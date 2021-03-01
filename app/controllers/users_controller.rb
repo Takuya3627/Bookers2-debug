fa7b10086143aa6d95653e01e4adf4f299a5d184
Tkuya3627 <tm.0630.ppp@gmail.com>
2021-03-01 14:25:09 +0000

    Fix Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   before_action :ensure_correct_user, only: [:update]
 
   def show
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -24,6 +26,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations',
+      sessions: 'users/sessions'
+  }
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :books
+  resources :users,only: [:show,:index,:edit,:update]
+end
